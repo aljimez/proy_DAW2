b8ad29d0df47e5bdd02e7e626c3744095014aabb
Alejandro Jimenez <ajimjal4@gmail.com>
2021-11-15 14:17:04 +0100

    web scrapping y inicio pagos

--- py/scrapping.py
+++ py/scrapping.py
@@ -5,5 +5,5 @@ from bs4 import BeautifulSoup
 url = 'http://rodalies.gencat.cat/es/alteracions_del_servei/'
 html = urllib.request.urlopen(url)
 soup2 = BeautifulSoup (html)
-tags = soup2('ul [class="list-group llistat_destacat_icones_cont"]')
+tags = soup2.find_all(class_="list-group llistat_destacat_icones_cont")
 print(tags)
