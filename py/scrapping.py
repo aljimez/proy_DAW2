2293890c68133f24babbe4043d95f2ffbabd4b67
Alejandro Jimenez <ajimjal4@gmail.com>
2021-11-17 09:38:29 +0100

    preparacion jupyter webscrapping

--- py/scrapping.py
+++
@@ -1,9 +0,0 @@
-import sys
-#!{sys.executable} -m pip install pandas
-import urllib
-from bs4 import BeautifulSoup
-url = 'http://rodalies.gencat.cat/es/alteracions_del_servei/'
-html = urllib.request.urlopen(url)
-soup2 = BeautifulSoup (html)
-tags = soup2.find_all(class_="list-group llistat_destacat_icones_cont")
-print(tags)
