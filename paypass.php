c4da4d7b554d3e0ca2b654eff4c56049813b72c9
Alejandro Jimenez <ajimjal4@gmail.com>
2021-11-29 13:49:20 +0100

    Create paypass.php

---
+++ paypass.php
@@ -0,0 +1,8 @@
+<?php
+//https://www.jose-aguilar.com/blog/como-implementar-una-pasarela-de-pago-mediante-tarjeta-de-credito-con-php/
+//https://stripe.com/docs/api?lang=php
+//https://api.stripe.com
+
+
+
+?>
