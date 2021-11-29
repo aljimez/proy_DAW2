a456373128fbe82aee0ca34ea4664e89cc3a176c
Alejandro Jimenez <ajimjal4@gmail.com>
2021-11-29 14:13:23 +0100

    Update paypass.php

--- paypass.php
+++ paypass.php
@@ -2,6 +2,7 @@
 //https://www.jose-aguilar.com/blog/como-implementar-una-pasarela-de-pago-mediante-tarjeta-de-credito-con-php/
 //https://stripe.com/docs/api?lang=php
 //https://api.stripe.com
+
 \Stripe\Stripe::setApiKey('sk_test_CGGvfNiIPwLXiDwaOfZ3oX6Y');
 
 \Stripe\PaymentIntent::create([
