e8d588ff74268576da141a5c74857297ef9cb04a
Alejandro Jimenez <ajimjal4@gmail.com>
2021-11-29 14:26:14 +0100

    Update paypass.php

--- paypass.php
+++ paypass.php
@@ -2,8 +2,10 @@
 //https://www.jose-aguilar.com/blog/como-implementar-una-pasarela-de-pago-mediante-tarjeta-de-credito-con-php/
 //https://stripe.com/docs/api?lang=php
 //https://api.stripe.com
+//test priv key: sk_test_51K19jHDizxitGBJXKV5vtfbxuUqBayxSwdOkngbO64DKND79xxL3hUfTfPQph8KgSPU5UB3WUaLC7MqEParwV0qH00giHl8AY8
+// test pub key: pk_test_51K19jHDizxitGBJXaGyiR3otX9b4JX1eqJwBC3i4IeVcAn4jiLzhRNBqicnRtgnHWW7BwRjkJJNE3sbUWL5GxuhX00SCA66sMZ
 
-\Stripe\Stripe::setApiKey('sk_test_CGGvfNiIPwLXiDwaOfZ3oX6Y');
+\Stripe\Stripe::setApiKey('pk_test_51K19jHDizxitGBJXaGyiR3otX9b4JX1eqJwBC3i4IeVcAn4jiLzhRNBqicnRtgnHWW7BwRjkJJNE3sbUWL5GxuhX00SCA66sMZ');
 
 \Stripe\PaymentIntent::create([
 'amount'=> 100,
