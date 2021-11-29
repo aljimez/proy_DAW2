80a18e476edd9dbf0f0750766a20dcf6f254da91
Alejandro Jimenez <ajimjal4@gmail.com>
2021-11-29 14:10:25 +0100

    stripe

--- paypass.php
+++ paypass.php
@@ -2,7 +2,13 @@
 //https://www.jose-aguilar.com/blog/como-implementar-una-pasarela-de-pago-mediante-tarjeta-de-credito-con-php/
 //https://stripe.com/docs/api?lang=php
 //https://api.stripe.com
+\Stripe\Stripe::setApiKey('sk_test_CGGvfNiIPwLXiDwaOfZ3oX6Y');
 
-
+\Stripe\PaymentIntent::create([
+'amount'=> 100,
+'currency'=> 'usd',
+'payment_method_type' => ['card'],
+'receipt_email' => 'ajimenezal01@gmail.com'
+]);
 
 ?>
