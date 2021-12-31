1ad98c8638a01d5883b7b4d77cfc02a251bf2eba
Alejandro Jimenez <ajimjal4@gmail.com>
2021-12-31 12:58:11 +0100

    inicio api

--- index.php
+++ index.php
@@ -4,21 +4,27 @@ include("menu.php");
 <!--Formulario consulta horarios-->
 <!--API de estaciones de rodalies cataluña https://data.renfe.com/dataset/estaciones-rodalies-barcelona/resource/dd090aa2-7b8d-45ab-97c2-bf70653bc117-->
 <div class="cont">
-<div class="inf_h">
-    <form action="horar.php" method="POST">
-        <h3>On vols anar?</h3>
-        <label for="orig">Estació d'origen</label>
-        <input type="text" id="orig" name="st_ori"><br><br>
-        <!--https://data.renfe.com/api/3/action/datastore_search_sql?sql=SELECT DESCRIPCION from "dd090aa2-7b8d-45ab-97c2-bf70653bc117" -->
+    <div class="inf_h">
+        <form action="horar.php" method="POST">
+            <h3>On vols anar?</h3>
+            <label for="orig">Estació d'origen</label>
+            <input type="text" id="orig" name="st_ori"><br><br>
+            <?php
+            
+            //Extraer descripcion, población y calle
+            //$sql="SELECT DESCRIPCION from 'dd090aa2-7b8d-45ab-97c2-bf70653bc117'";
+            //https://data.renfe.com/api/3/action/datastore_search_sql?$sql
 
+            ?>
 
-        <label for="dest">Estació destí</label>
-        <input type="text" id="dest" name="st_dst"><br><br>
-        <label for="mome">Quan?</label><input type="date" id="mome">
-        <br><br><br>
-        <input type="Submit" value="Consultar">
-    </form>
+            <label for="dest">Estació destí</label>
+            <input type="text" id="dest" name="st_dst"><br><br>
+            <label for="mome">Quan?</label><input type="date" id="mome">
+            <br><br><br>
+            <input type="Submit" value="Consultar">
+        </form>
     </div>
 </div>
 </body>
+
 </html>
