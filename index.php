e5459c96e1096151053fcc5f1c5898b61bcd4279
Alejandro Jimenez <ajimenezal01@gmail.com>
2022-03-14 14:19:38 +0100

    form rec php

--- index.php
+++ index.php
@@ -11,21 +11,22 @@ include("menu.php");
   <div class="cont">
     <div class="inf_h">
       <form action="horar.php" method="POST">
-      <label for="email">Correu electronic</label>
-      <input type="text" name="email">
-      <label for="">Nom</label>
-      <input type="text">
-      <label for="">Tipus de reclamació</label>
-      <select name="" id="">
-<option value="">Justificante de retraso</option>
-<option value="">Consutas y reclamaciones</option>
-<option value="">Atención telefonica</option>
-<option value="">Solicitud de objetos perdidos</option>
-<option value="">Devolución Xpress</option>
-      </select><br>
-      <textarea type="text"rows="10" cols="150">
-        Escriu aqui la teva reclamació
-      </textarea>
+        <label for="email">Correu electronic</label>
+        <input type="email" name="email">
+        <label for="name">Nom</label>
+        <input type="text" id="name">
+        <label for="recty">Tipus de reclamació</label>
+        <select name="reclam" id="recty">
+        <option value="jret">Justificante de retraso</option>
+        <option value="corec">Consutas y reclamaciones</option>
+        <option value="tfatt">Atención telefonica</option>
+        <option value="lossob">Solicitud de objetos perdidos</option>
+        <option value="xpdev">Devolución Xpress</option>
+        </select><br>
+        <textarea type="text"rows="10" cols="150" name="comm">
+          Escriu aqui la teva reclamació
+        </textarea><br>
+        <button type="submit">Enviar</button>
       </form>
     </div>
   </div>
